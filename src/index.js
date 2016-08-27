90d50181d552463e724aefdb9c9e72d5f837fbc3
Mick0n <mikael.norrman@hotmail.se>
2016-08-27 14:17:41 +0200

    Initial code commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+var config = require('../config');
+var needle = require('needle');
+var express = require('express');
+
+const baseUrl = 'https://api.twitch.tv/kraken';
+
+const headers = {
+	'Content-Type': 'application/json'
+};
+
+const defaultOptions = {
+	compressed: true,
+	rejectUnauthorized: true,
+	headers: headers
+};
+
+needle.get(baseUrl, defaultOptions, (error, response)=> {
+	if (error) console.log(error);
+	if (response.body) console.log(response.body);
+});
+
+var app = new express();
+
+app.get('/', (req, res) => {
+	res.end();
+});
+
+var server = app.listen(8000, () => {
+	console.log('Server started on port 8000');
+});
